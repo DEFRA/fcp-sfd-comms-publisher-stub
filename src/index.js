aebc6482de4af66ec3ff099d2407b73c3fbb8923
Rana Salem <ranatasalem@gmail.com>
2025-11-06 15:05:47 +0000

    resolve start up errors

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import process from 'node:process'
 import { createLogger } from './common/helpers/logging/logger.js'
 import { startServer } from './common/helpers/start-server.js'
 
+const logger = createLogger()
+
 await startServer()
 
-process.on('unhandledRejection', (error) => {
-  const logger = createLogger()
+process.on('unhandledRejection', (err) => {
   logger.info('Unhandled rejection')
-  logger.error(error)
+  logger.error(err)
   process.exitCode = 1
 })
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import Hapi from '@hapi/hapi'
+import Joi from 'joi'
 
 import { secureContext } from '@defra/hapi-secure-context'
 
@@ -38,12 +39,8 @@ async function createServer () {
     }
   })
 
-  // Hapi Plugins:
-  // requestLogger  - automatically logs incoming requests
-  // requestTracing - trace header logging and propagation
-  // secureContext  - loads CA certificates from environment config
-  // pulse          - provides shutdown handlers
-  // router         - routes used in the app
+  server.validator(Joi)
+
   await server.register([
     requestLogger,
     requestTracing,
