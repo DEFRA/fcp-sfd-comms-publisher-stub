b4390626bcbea6acb46c60ee3f08c2c9bc36933e
Natacha Tomkinson <natacha.tomkinson@defra.gov.uk>
2025-11-26 15:57:00 +0000

    fix(#SFD2-507): change localstack ports to non-default for multiple instances of localstack to run locally

--- localstack/localstack.sh
+++ localstack/localstack.sh
@@ -4,14 +4,15 @@ echo "configuring sqs and sns"
 echo "==================="
 LOCALSTACK_HOST=localhost
 AWS_REGION=eu-west-2
+PORT=4568
 
 create_queue() {
   local QUEUE_NAME_TO_CREATE=$1
   local DLQ_NAME="${QUEUE_NAME_TO_CREATE}-deadletter"
   
-  awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 sqs create-queue --queue-name ${DLQ_NAME} --region ${AWS_REGION} --attributes VisibilityTimeout=60
-  local DLQ_ARN=$(awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 sqs get-queue-attributes --queue-url http://sqs.${AWS_REGION}.${LOCALSTACK_HOST}.localstack.cloud:4566/000000000000/${DLQ_NAME} --attribute-names QueueArn --query 'Attributes.QueueArn' --output text)
-  awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 sqs create-queue --queue-name ${QUEUE_NAME_TO_CREATE} --region ${AWS_REGION} --attributes '{
+  awslocal --endpoint-url=http://${LOCALSTACK_HOST}:${PORT} sqs create-queue --queue-name ${DLQ_NAME} --region ${AWS_REGION} --attributes VisibilityTimeout=60
+  local DLQ_ARN=$(awslocal --endpoint-url=http://${LOCALSTACK_HOST}:${PORT} sqs get-queue-attributes --queue-url http://sqs.${AWS_REGION}.${LOCALSTACK_HOST}.localstack.cloud:${PORT}/000000000000/${DLQ_NAME} --attribute-names QueueArn --query 'Attributes.QueueArn' --output text)
+  awslocal --endpoint-url=http://${LOCALSTACK_HOST}:${PORT} sqs create-queue --queue-name ${QUEUE_NAME_TO_CREATE} --region ${AWS_REGION} --attributes '{
     "VisibilityTimeout": "60",
     "RedrivePolicy": "{\"deadLetterTargetArn\":\"'${DLQ_ARN}'\",\"maxReceiveCount\":\"3\"}"
   }'
@@ -19,14 +20,14 @@ create_queue() {
 
 create_topic() {
   local TOPIC_NAME_TO_CREATE=$1
-  awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 sns create-topic --name ${TOPIC_NAME_TO_CREATE} --region ${AWS_REGION}
+  awslocal --endpoint-url=http://${LOCALSTACK_HOST}:${PORT} sns create-topic --name ${TOPIC_NAME_TO_CREATE} --region ${AWS_REGION}
 }
 
 subscribe_queue_to_topic() {
   local TOPIC=$1
   local QUEUE=$2
-  local QUEUE_ARN=$(awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 sqs get-queue-attributes --queue-url http://sqs.${AWS_REGION}.${LOCALSTACK_HOST}.localstack.cloud:4566/000000000000/${QUEUE} --attribute-names QueueArn --query 'Attributes.QueueArn' --output text)
-  awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 sns subscribe --topic-arn arn:aws:sns:${AWS_REGION}:000000000000:${TOPIC} --protocol sqs --notification-endpoint ${QUEUE_ARN} --region ${AWS_REGION}
+  local QUEUE_ARN=$(awslocal --endpoint-url=http://${LOCALSTACK_HOST}:${PORT} sqs get-queue-attributes --queue-url http://sqs.${AWS_REGION}.${LOCALSTACK_HOST}.localstack.cloud:${PORT}/000000000000/${QUEUE} --attribute-names QueueArn --query 'Attributes.QueueArn' --output text)
+  awslocal --endpoint-url=http://${LOCALSTACK_HOST}:${PORT} sns subscribe --topic-arn arn:aws:sns:${AWS_REGION}:000000000000:${TOPIC} --protocol sqs --notification-endpoint ${QUEUE_ARN} --region ${AWS_REGION}
 }
 
 create_queue "fcp_sfd_comms_request"
